0104c58b7b605bf69987f6e2cda0e1a40c66906f
18Ruslan18 <79464524+18Ruslan18@users.noreply.github.com>
2021-03-18 15:17:24 +0300

    new Jpanel

--- src/com/company/SimpleGUI.java
+++ src/com/company/SimpleGUI.java
@@ -3,29 +3,34 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 public class SimpleGUI extends JFrame {
-    private JButton button = new JButton("Press");
+    private JButton buttonStart = new JButton("Запуск");
+    private JButton buttonEditLine = new JButton("Новые отрезки");
+    private JButton buttonEditSquare = new JButton("Новое окно");
     private JTextField textField = new JTextField("",5);
     private JLabel label = new JLabel("Input:");
     private JRadioButton radioButton1 = new JRadioButton("Select this");
     private JRadioButton radioButton2 = new JRadioButton("Select that");
     private JCheckBox checkBox = new JCheckBox("Check", false);
+    private JPanel container = new JPanel(new GridLayout(1,3,2,2));
     public SimpleGUI(){
+
         super("Simple Example");
-        this.setBounds(100,100,250,100);
+        this.setBounds(100,100,700,700);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Container container = this.getContentPane();
-        container.setLayout(new GridLayout(3,2,2,2));
-        container.add(label);
-        container.add(textField);
-        ButtonGroup buttonGroup = new ButtonGroup();
-        buttonGroup.add(radioButton1);
-        buttonGroup.add(radioButton2);
-        container.add(radioButton1);
-        radioButton1.setSelected(true);
-        container.add(radioButton2);
-        container.add(checkBox);
-        button.addActionListener(new ButtonEventListner());
-        container.add(button);
+        //container.add(label);
+        //container.add(textField);
+        ///ButtonGroup buttonGroup = new ButtonGroup();
+        //buttonGroup.add(radioButton1);
+        //buttonGroup.add(radioButton2);
+        //container.add(radioButton1);
+        //radioButton1.setSelected(true);
+        //container.add(radioButton2);
+        //container.add(checkBox);
+        buttonStart.addActionListener(new ButtonEventListner());
+        container.add(buttonStart);
+        container.add(buttonEditLine);
+        container.add(buttonEditSquare);
+        add(container, BorderLayout.SOUTH);
     }
 class ButtonEventListner implements  ActionListener{
         public void actionPerformed(ActionEvent event){
